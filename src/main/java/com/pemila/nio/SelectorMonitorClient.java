449ceee1fc8a8a913fecf1047ec32a90fd5a3a53
pemila <pemila@126.com>
2019-05-17 11:42:39 +0800

    ServerSocketChannel添加监听端口，创建socket客户端进行测试

---
+++ src/main/java/com/pemila/nio/SelectorMonitorClient.java
@@ -0,0 +1,22 @@
+package com.pemila.nio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * socket客户端
+ * @author 月在未央
+ * @date 2019/5/17 9:57
+ */
+public class SelectorMonitorClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 30000);
+        OutputStream os = socket.getOutputStream();
+        PrintWriter print = new PrintWriter(os);
+        print.write("hello,server!");
+        print.close();
+        os.close();
+    }
+}
--- src/main/java/com/pemila/nio/SelectorMonitorDemo.java
+++ src/main/java/com/pemila/nio/SelectorMonitorDemo.java
@@ -1,14 +1,15 @@
 package com.pemila.nio;
 
 import java.io.IOException;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.net.InetSocketAddress;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
 /**
- * 注册时间到Selector上并监听事件
+ * 创建ServerSocket
+ * 获取ServerSocketChannel
+ * 注册通道到Selector上并监听事件
  * @author 月在未央
  * @date 2019/5/16 18:17
  */
@@ -17,12 +18,14 @@ public class SelectorMonitorDemo {
     public static void main(String[] args) throws IOException {
         // 创建Selector
         Selector selector = Selector.open();
-        // 创建一个Channel
+        // 打开一个ServerSocketChannel通道
         ServerSocketChannel channel = ServerSocketChannel.open();
+        // 为通道设定监听地址
+        channel.bind(new InetSocketAddress("127.0.0.1",30000));
         // 设置Channel为非阻塞
         channel.configureBlocking(false);
         // 将channel注册到Selector上并监听ACCPET事件
-        SelectionKey key = channel.register(selector,SelectionKey.OP_ACCEPT);
+        SelectionKey key = channel.register(selector,SelectionKey.OP_ACCEPT,"ServerSocket-test");
         while (true){
             // 获取当前就绪的通道数
             int readChannels = selector.select();
@@ -35,13 +38,14 @@ public class SelectorMonitorDemo {
             while (keyIterator.hasNext()){
                 //已就绪通道处理
                 SelectionKey selectionKey = (SelectionKey) keyIterator.next();
-                if(key.isAcceptable()) {
+                if(selectionKey.isAcceptable()) {
                     // a connection was accepted by a ServerSocketChannel.
-                } else if (key.isConnectable()) {
+                    System.out.println(selectionKey.attachment()+" accepted!");
+                } else if (selectionKey.isConnectable()) {
                     // a connection was established with a remote server.
-                } else if (key.isReadable()) {
+                } else if (selectionKey.isReadable()) {
                     // a channel is ready for reading
-                } else if (key.isWritable()) {
+                } else if (selectionKey.isWritable()) {
                     // a channel is ready for writing
                 }
                 // 处理结束从集合中移除
